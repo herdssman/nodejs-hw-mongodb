16b50b78cfac8a6cdb54aaf82f515427a8ede7fd
herdssman <matthew.hf@outlook.com>
2025-10-18 15:41:54 +0200

    photos

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,8 +17,8 @@ export const SMTP = {
 };
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'tmp');
-export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'tmp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'src', 'uploads');
 
 export const CLOUDINARY = {
   CLOUD_NAME: 'CLOUD_NAME',
